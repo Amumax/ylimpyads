62a9451146dd58fd611eb08c1e9fa6b2d1e67769
Maxim <naidenov.ma@phystech.edu>
2023-05-21 17:32:25 +0300

    Dummy about page

---
+++ frontend/src/About.tsx
@@ -0,0 +1,11 @@
+import './App.css'
+
+function About() {
+    return (
+        <div>
+            <span>About</span>
+        </div>
+    )
+}
+
+export default About;
--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -1,6 +1,7 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App'
+import About from './About'
 
 import './index.css'
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
@@ -12,6 +13,7 @@ ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
       <Routes>
         <Route path='/admin/*' element={<App/>}/>
         <Route path='/' element={<MyResponsiveCalendarCanvas/>}/>
+        <Route path='/about' element={<About/>}/>
       </Routes>
     </BrowserRouter>
   </React.StrictMode>,
